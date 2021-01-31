eb42349c2a9d02a59fd206b121a04b2787448b7a
Thoritie <thohirh9@gmail.com>
2021-01-31 14:40:44 +0700

    :zap: Second Fragment :snowman:

--- app/src/main/java/com/example/androidhandson/fragment/FirstFragment.kt
+++ app/src/main/java/com/example/androidhandson/fragment/FirstFragment.kt
@@ -1,14 +1,22 @@
 package com.example.androidhandson.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.androidhandson.R
+import com.example.androidhandson.databinding.ActivityOneBinding
+import com.example.androidhandson.databinding.FragmentFirstBinding
 
 
 class FirstFragment : BaseFragment() {
+    private val binding: FragmentFirstBinding by lazy {
+        FragmentFirstBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +25,20 @@ class FirstFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        Log.i(logTag, "onCreateView")
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+     binding.btnNext.setOnClickListener {
+         (activity as? ExampleFragmentActivity)?.run {
+             supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                 replace<SecondFragment>(R.id.fragment_container_view)
+                 addToBackStack(null)
+             }
+         }
+     }
     }
 }
