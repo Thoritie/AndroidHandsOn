eac9ebef52c1ab3adb72c7f93edaf2920b5a8158
Thoritie <thohirh9@gmail.com>
2021-01-31 13:09:39 +0700

    :memo: day #1 MainActivity :snowman:

---
+++ app/src/main/java/com/example/androidhandson/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.androidhandson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.androidhandson.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val oddsAdapter: OddsAdapter by lazy {
+        OddsAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setUpView()
+    }
+
+    private fun setUpView() {
+        binding.rvOdds.adapter = oddsAdapter
+        binding.rvOdds.layoutManager = LinearLayoutManager(this)
+        oddsAdapter.update(mockOddsModel())
+    }
+
+    private fun mockOddsModel(): List<OddsModel> {
+        return listOf(
+            OddsModel(
+                image = R.drawable.logo_odds,
+                name = "Name 1"
+            ),
+            OddsModel(
+                image = R.drawable.logo_odds,
+                name = "Name 2"
+            )
+        )
+    }
+}
